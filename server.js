f4b8182f21b5ed11fc2b13b4078f3809b2119614
DUC-THINH <buiducthinh56@gmail.com>
2023-02-12 01:58:59 +0700

    add firebase

--- server.js
+++ server.js
@@ -3,10 +3,24 @@ const app = express();
 const port = process.env.PORT
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger/swagger.json');
+const {db} = require("./firebase/firebase.ts")
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+const docRef = db.collection('users').doc('fyawvl5eUVLoFHpnjfZF');
+
+docRef.get().then(doc => {
+  if (!doc.exists) {
+    console.log('No such document!');
+  } else {
+    console.log(doc.data());
+  }
+})
+.catch(err => {
+  console.log('Error getting document', err);
 });
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
