34e9e3765b8ff2c35adbd8c911da32119b1a4d4a
DUC-THINH <buiducthinh56@gmail.com>
2023-02-11 15:54:07 +0700

    add swagger

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express') 
 const app = express();
 const port = process.env.PORT
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const swaggerAutogen = require('swagger-autogen');
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
